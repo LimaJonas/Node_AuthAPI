8cf0a7bc059b4d647c5a0880562d0c4aa55bf0e7
Jonas Lima <jonaslima015@gmail.com>
2021-01-20 16:10:36 -0300

    iniciado função para procurar usuario no banco

--- README.md
+++ README.md
@@ -5,6 +5,10 @@
 npm install
 ```
 
+## Database
+- SQL COMMAND "CREATE DATABASE nodeapi_development"
+- NPM COMMAND "npx sequelize db:migrate"
+
 ### Run server
 ```
 npm start
--- src/features/auth/controllers.js
+++ src/features/auth/controllers.js
@@ -1,6 +1,14 @@
+const Boom = require('boom')
+const services = require('./services')
+
 module.exports = {
-    auth: ctx => {
-        const { response } = ctx
-        ctx.body = { message: 'AUTH SUCCESS'}
+    auth: async ctx => {
+        const { request, response } = ctx
+        const user = await services.auth(request.body)
+        if(user){
+            response.body = user
+        } else{
+            response.body = { result: Boom.badRequest() }
+        }
     }
 }
